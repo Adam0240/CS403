b7672c2a3fa83acea981061c637cc3fd174e0796
Adam <adam.adusky@gmail.com>
2025-08-26 10:48:09 -0400

    Initial commit

---
+++ leekcode_bfs/bin/leekcode_bfs.dart
@@ -0,0 +1,5 @@
+import 'package:leekcode_bfs/leekcode_bfs.dart' as leekcode_bfs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leekcode_bfs.calculate()}!');
+}
---
+++ leekcode_bfs/test/leekcode_bfs_test.dart
@@ -0,0 +1,8 @@
+import 'package:leekcode_bfs/leekcode_bfs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
